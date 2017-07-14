0267d7c29464f9165d89f4955baba332374911ff
Mariusz Dziecielski <mariuszdziec@gmail.com>
2017-07-14 07:04:00 +0200

    Optimize js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 $(function () {
     var url = 'https://restcountries.eu/rest/v1/name/',
-        countriesList = $('#countries');
+        $countriesList = $('#countries'),
+        $inputElement = $('#country-name');
     $('#search').click(searchCountries);
-    $('#country-name').focus(function () {
-        $('#country-name').val('');
+    $inputElement.focus(function () {
+        $inputElement.val('');
     });
     function searchCountries() {
-        var countryName = $('#country-name').val();
+        var countryName = $inputElement.val();
         if (!countryName.length) countryName = 'Poland';
         $.ajax({
             url: url + countryName,
@@ -15,21 +16,21 @@ $(function () {
         });
     }
     function showCountriesList(resp) {
-        countriesList.empty();
+        $countriesList.empty();
         resp.forEach(function (item) {
-            $('<li class="name">').html('<h3>' + item.name + '</h3>').appendTo(countriesList).fadeIn('slow');
-            $('<li class="info">').text('Background Information:').appendTo(countriesList).fadeIn('slow');
-            $('<li>').html('<span>Capital: </span>' + item.capital).appendTo(countriesList).fadeIn('slow');
-            $('<li>').html('<span>Land area: </span>' + item.area).appendTo(countriesList).fadeIn('slow');
-            $('<li>').html('<span>Population: </span>' + item.population).appendTo(countriesList).fadeIn('slow');
-            $('<li>').html('<span>Native Name: </span>' + item.nativeName).appendTo(countriesList).fadeIn('slow');
-            $('<li>').html('<span>Region: </span>' + item.region).appendTo(countriesList).fadeIn('slow');
-            $('<li>').html('<span>Subregion: </span>' + item.subregion).appendTo(countriesList).fadeIn('slow');
-            $('<li>').html('<span>Top Level Domain: </span>' + item.topLevelDomain[0]).appendTo(countriesList).fadeIn('slow');
-            $('<li>').html('<span>Calling Code: </span>' + item.callingCodes[0]).appendTo(countriesList).fadeIn('slow');
-            $('<li>').html('<span>Currency: </span>' + item.currencies[0]).appendTo(countriesList).fadeIn('slow');
-            $('<li>').html('<span>Language: </span>' + item.languages[0]).appendTo(countriesList).fadeIn('slow');
-            $('<li>').html('<span>Alpha 3 Code: </span>' + item.alpha3Code).appendTo(countriesList).fadeIn('slow');
+            $('<li class="name">').html('<h3>' + item.name + '</h3>').appendTo($countriesList).fadeIn('slow');
+            $('<li class="info">').text('Background Information:').appendTo($countriesList).fadeIn('slow');
+            $('<li>').html('<span>Capital: </span>' + item.capital).appendTo($countriesList).fadeIn('slow');
+            $('<li>').html('<span>Land area: </span>' + item.area).appendTo($countriesList).fadeIn('slow');
+            $('<li>').html('<span>Population: </span>' + item.population).appendTo($countriesList).fadeIn('slow');
+            $('<li>').html('<span>Native Name: </span>' + item.nativeName).appendTo($countriesList).fadeIn('slow');
+            $('<li>').html('<span>Region: </span>' + item.region).appendTo($countriesList).fadeIn('slow');
+            $('<li>').html('<span>Subregion: </span>' + item.subregion).appendTo($countriesList).fadeIn('slow');
+            $('<li>').html('<span>Top Level Domain: </span>' + item.topLevelDomain[0]).appendTo($countriesList).fadeIn('slow');
+            $('<li>').html('<span>Calling Code: </span>' + item.callingCodes[0]).appendTo($countriesList).fadeIn('slow');
+            $('<li>').html('<span>Currency: </span>' + item.currencies[0]).appendTo($countriesList).fadeIn('slow');
+            $('<li>').html('<span>Language: </span>' + item.languages[0]).appendTo($countriesList).fadeIn('slow');
+            $('<li>').html('<span>Alpha 3 Code: </span>' + item.alpha3Code).appendTo($countriesList).fadeIn('slow');
         });
     }
 });
