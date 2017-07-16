27a0f68b6e903a1325c1e2b61e15d61730dd1c03
Mariusz Dziecielski <mariuszdziec@gmail.com>
2017-07-16 14:35:11 +0200

    Optimize js

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,67 @@
+/* jshint -W043 */
 $(function () {
-    var url = 'https://restcountries.eu/rest/v1/name/',
-        $countriesList = $('#countries'),
-        $inputElement = $('#country-name');
-    $('#search').click(searchCountries);
+    var url = "https://restcountries.eu/rest/v1/name/",
+        $countriesList = $("#countries"),
+        $inputElement = $("#country-name");
+    $("#search").click(searchCountries);
     $inputElement.focus(function () {
-        $inputElement.val('');
+        $inputElement.val("");
     });
     function searchCountries() {
         var countryName = $inputElement.val();
-        if (!countryName.length) countryName = 'Poland';
+        if (!countryName.length) countryName = "Poland";
         $.ajax({
             url: url + countryName,
-            method: 'GET',
+            method: "GET",
             success: showCountriesList
         });
     }
     function showCountriesList(resp) {
+        var searchResult = "";
         $countriesList.empty();
         resp.forEach(function (item) {
-            $('<li class="name">').html('<h3>' + item.name + '</h3>').appendTo($countriesList).fadeIn('slow');
-            $('<li class="info">').text('Background Information:').appendTo($countriesList).fadeIn('slow');
-            $('<li>').html('<span>Capital: </span>' + item.capital).appendTo($countriesList).fadeIn('slow');
-            $('<li>').html('<span>Land area: </span>' + item.area).appendTo($countriesList).fadeIn('slow');
-            $('<li>').html('<span>Population: </span>' + item.population).appendTo($countriesList).fadeIn('slow');
-            $('<li>').html('<span>Native Name: </span>' + item.nativeName).appendTo($countriesList).fadeIn('slow');
-            $('<li>').html('<span>Region: </span>' + item.region).appendTo($countriesList).fadeIn('slow');
-            $('<li>').html('<span>Subregion: </span>' + item.subregion).appendTo($countriesList).fadeIn('slow');
-            $('<li>').html('<span>Top Level Domain: </span>' + item.topLevelDomain[0]).appendTo($countriesList).fadeIn('slow');
-            $('<li>').html('<span>Calling Code: </span>' + item.callingCodes[0]).appendTo($countriesList).fadeIn('slow');
-            $('<li>').html('<span>Currency: </span>' + item.currencies[0]).appendTo($countriesList).fadeIn('slow');
-            $('<li>').html('<span>Language: </span>' + item.languages[0]).appendTo($countriesList).fadeIn('slow');
-            $('<li>').html('<span>Alpha 3 Code: </span>' + item.alpha3Code).appendTo($countriesList).fadeIn('slow');
+            searchResult += "\
+                <li class='name'> \
+                    <h3>" + item.name + "</h3>" + "\
+                </li> \
+                <li class='info'> \
+                    Background Information: \
+                </li> \
+                <li> \
+                    <span>Capital: </span>" + item.capital + "\
+                </li> \
+                <li> \
+                    <span>Land area: </span>" + item.area + "\
+                </li> \
+                <li> \
+                    <span>Population: </span>" + item.population + "\
+                </li> \
+                <li> \
+                    <span>Native Name: </span>" + item.nativeName + "\
+                </li> \
+                <li> \
+                    <span>Region: </span>" + item.region + "\
+                </li> \
+                <li> \
+                    <span>Subregion: </span>" + item.subregion + "\
+                </li> \
+                <li> \
+                    <span>Top Level Domain: </span>" + item.topLevelDomain[0] + "\
+                </li> \
+                <li> \
+                    <span>Calling Code: </span>" + item.callingCodes[0] + "\
+                </li> \
+                <li> \
+                    <span>Currency: </span>" + item.currencies[0] + "\
+                </li> \
+                <li> \
+                    <span>Language: </span>" + item.languages[0] + "\
+                </li> \
+                <li> \
+                    <span>Alpha 3 Code: </span>" + item.alpha3Code + "\
+                </li> \
+            ";
         });
+        $(searchResult).appendTo($countriesList).fadeIn("slow");
     }
 });
