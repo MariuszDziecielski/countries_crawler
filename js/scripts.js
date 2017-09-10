0d5e8d7479ae6b75778cf62746ca72a80852b850
Mariusz Dziecielski <mariuszdziec@gmail.com>
2017-09-10 08:38:55 +0200

    Remove unnecessary spaces & change js

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,71 @@
 $(function () {
-    const url = "https://restcountries.eu/rest/v1/name/",
-        $countriesList = $("#countries"),
-        $inputElement = $("#country-name");
-    $("#search").click(searchCountries);
-    $inputElement.focus(function () {
-        $inputElement.val('');
-    });
-    function searchCountries() {
-        let countryName = $inputElement.val();
-        if (!countryName.length) countryName = 'Poland';
-        $.ajax({
-            url: url + countryName,
-            method: "GET",
-            success: showCountriesList
+    function CountriesCrawler(url, element) {
+        this.url = url;
+        this.$countriesList = element;
+        this.$inputElement = $("#country-name");
+        this.$inputElement.focus(() => {
+            this.$inputElement.val('');
         });
+        $("#search").click(this.searchCountries);
     }
-    function showCountriesList(resp) {
-        let searchResult = "";
-        $countriesList.empty();
-        resp.forEach(function (item) {
-            searchResult += '\
-                <li class="name"> \
-                    <h3>' + item.name + '</h3> \
-                </li> \
-                <li class="info"> \
-                    Background Information: \
-                </li> \
-                <li> \
-                    <span>Capital: </span>' + item.capital + '\
-                </li> \
-                <li> \
-                    <span>Land area: </span>' + item.area + '\
-                </li> \
-                <li> \
-                    <span>Population: </span>' + item.population + '\
-                </li> \
-                <li> \
-                    <span>Native Name: </span>' + item.nativeName + '\
-                </li> \
-                <li> \
-                    <span>Region: </span>' + item.region + '\
-                </li> \
-                <li> \
-                    <span>Subregion: </span>' + item.subregion + '\
-                </li> \
-                <li> \
-                    <span>Top Level Domain: </span>' + item.topLevelDomain[0] + '\
-                </li> \
-                <li> \
-                    <span>Calling Code: </span>' + item.callingCodes[0] + '\
-                </li> \
-                <li> \
-                    <span>Currency: </span>' + item.currencies[0] + '\
-                </li> \
-                <li> \
-                    <span>Language: </span>' + item.languages[0] + '\
-                </li> \
-                <li> \
-                    <span>Alpha 3 Code: </span>' + item.alpha3Code + '\
-                </li> \
-            ';
-        });
-        $(searchResult).appendTo($countriesList).fadeIn("slow");
-    }
+    CountriesCrawler.prototype = {
+        searchCountries: function () {
+            let countryName = countriesCrawler.$inputElement.val();
+            if (!countryName.length) countryName = 'Poland';
+            $.ajax({
+                url: `${countriesCrawler.url}${countryName}`,
+                method: "GET",
+                success: countriesCrawler.showCountriesList
+            });
+        },
+        showCountriesList: function (resp) {
+            let searchResult = "";
+            countriesCrawler.$countriesList.empty();
+            resp.forEach(function (item) {
+                searchResult += `\
+                    <li class="name">\
+                        <h3>${item.name}</h3>\
+                    </li>\
+                    <li class="info">\
+                        Background Information:\
+                    </li>\
+                    <li>\
+                        <span>Capital: </span>${item.capital}\
+                    </li>\
+                    <li>\
+                        <span>Land area: </span>${item.area}\
+                    </li>\
+                    <li>\
+                        <span>Population: </span>${item.population}\
+                    </li>\
+                    <li>\
+                        <span>Native Name: </span>${item.nativeName}\
+                    </li>\
+                    <li>\
+                        <span>Region: </span>${item.region}\
+                    </li>\
+                    <li>\
+                        <span>Subregion: </span>${item.subregion}\
+                    </li>\
+                    <li>\
+                        <span>Top Level Domain: </span>${item.topLevelDomain[0]}\
+                    </li>\
+                    <li>\
+                        <span>Calling Code: </span>${item.callingCodes[0]}\
+                    </li>\
+                    <li>\
+                        <span>Currency: </span>${item.currencies[0]}\
+                    </li>\
+                    <li>\
+                        <span>Language: </span>${item.languages[0]}\
+                    </li>\
+                    <li>\
+                        <span>Alpha 3 Code: </span>${item.alpha3Code}\
+                    </li>\
+                `;
+            });
+            $(searchResult).appendTo(countriesCrawler.$countriesList).fadeIn("slow");
+        }
+    };
+    const countriesCrawler = new CountriesCrawler("https://restcountries.eu/rest/v1/name/", $("#countries"));
 });
